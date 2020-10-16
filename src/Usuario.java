b5e0b7d2a16334da7a490f83c4403cf696242e46
DSCamilo <72458548+DSCamilo@users.noreply.github.com>
2020-10-16 12:58:38 -0500

    Atributos clase Usuario
    Se crean atributos clase Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,4 +2,11 @@ package src;
 
 //Camilo
 public class Usuario {
+    
+    //Atributos usuario
+            
+    private int idUsuario;
+    private String nombreUsuario;
+    private String contraseña;
+    private String tipoUsuario;
 }
