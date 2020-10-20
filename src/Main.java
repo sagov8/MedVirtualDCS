634a029d749a8cf2ecbc0ba3d81db08aa68e4817
Sagov8 <sago.vivas@gmail.com>
2020-10-20 10:00:05 -0500

    Modificación clase médico y main
    Datos pasados por medio del constructor y uso de la clase Calendar para obtener una estampa del tiempo

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 package src;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Medico medico = new Medico();
-        medico.setIdMedico(1);
-        medico.setNombreMedico("Juan");
-        medico.setApellidoMedico("Perez");
-        medico.setNumeroTelefonicoMedico("3112345678");
-        medico.setCorreoMedico("juanperez@gmail.com");
-        medico.setEspecialidad("Medicina interna");
+        Medico medico = new Medico(1,"3112345678",
+                "Juan","Perez","abc@abs.com",
+                "Medicina interna");
 
         System.out.println(medico);
+        Calendar colombiaToday = Calendar.getInstance(TimeZone.getDefault());
+        System.out.println(colombiaToday.getTime());
     }
 }
